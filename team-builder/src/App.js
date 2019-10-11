7e37f564a8c3d19ee9052cf3db093323f4933cf0
David Francis <davidjayfrancis@gmail.com>
2019-10-11 11:49:33 -0700

    added Form component, and ability to upload information that is displayed to the website

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Form from "./components/Form.js";
+import Person from "./components/Person.js";
 
+let data = [
+  {
+    name: "David",
+    email: "davidjayfrancis@gmail.com",
+    role: "developer"
+  },
+  {
+    name: "Navid",
+    email: "coolnavid@gmail.com",
+    role: "supreme leader"
+  }
+];
 function App() {
+  const [team, setTeam] = React.useState(data);
+
+  const addPerson = p => {
+    const newPerson = {
+      username: p.username,
+      email: p.email,
+      role: p.role
+    };
+    setTeam([...team, newPerson]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Super Duper Form Capacitator</h1>
+        <Form addPersonFn={addPerson} />
+        {team.map(person => {
+          return (
+            <Person
+              name={person.username}
+              email={person.email}
+              role={person.role}
+            />
+          );
+        })}
       </header>
     </div>
   );
